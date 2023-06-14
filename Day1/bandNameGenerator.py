8271804a685d0d4ba4e16b830f6292677dcc9448
Sabbir Hossain Abir <2020-2-60-185@std.ewubd.edu>
2023-06-14 23:19:28 +0600

    Day 1

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band naming program")
+city_name = input("What is your city name?\n")
+pet_name = input("what is your pet name?\n")
+band_name = city_name+ " " + pet_name
+print("Name of you band is " + band_name)
